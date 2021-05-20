4f4b3ddf2ec5f23822b91551be1ca4a31b179474
phlgr <philipp@gartz.dev>
2021-05-20 17:07:48 +0200

    ✨ Add onClick to Square

--- src/app/components/Square.tsx
+++ src/app/components/Square.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Square(props: { value: number }): JSX.Element {
-  return <button className="square">{props.value}</button>;
+  return (
+    <button onClick={() => alert('click')} className="square">
+      {props.value}
+    </button>
+  );
 }
 
 export default Square;
